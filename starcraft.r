42c5a1f9525025004806a322e65d0423af27a984
FabriceCh <fabrice.charbonneau@hotmail.com>
2017-11-25 22:38:26 -0500

    ages fix csv

--- starcraft.r
+++ starcraft.r
@@ -1,2 +1,6 @@
-#comment
-#script
+setwd("C:/Users/fabrice/Documents/GitHub/sc2StatsLesStatsAStats")
+donnee <- read.csv("ExcelJoueurs.csv", header = TRUE, sep = ",")
+noms <- donnee$Joueur
+ages <- donnee$Age
+ages
+mean(ages)
